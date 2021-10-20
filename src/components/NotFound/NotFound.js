ff8b55e4ce4dcd10b6936f5dc69810477ac279a9
jannatulmawyajoya <jannatuljoya228@gmail.com>
2021-10-20 19:51:08 +0600

    done finally

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div className="row not-found-div">
             <div className="col-lg-12 ">
-                <img className="page-not-found" src="https://i.ibb.co/Pmz2kJN/page-not-found-14904975.jpg" alt="" />
+                <img className="page-not-found img-fluid" src="https://i.ibb.co/Pmz2kJN/page-not-found-14904975.jpg" alt="" />
                 <div>  <Link to="/"><button className="btn btn-primary mb-3">Go Back</button></Link></div>
               
             </div>
